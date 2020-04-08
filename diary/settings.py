a5032e208919f3ac5108d64f6e4b54e6018ec2cf
crypt0box <pray.happy76@gmail.com>
2020-04-08 18:05:30 +0900

    修正：細かい修正

--- diary/settings.py
+++ diary/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'diary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'name',
-        'USER': 'user',
-        'PASSWORD': '',
-        'HOST': 'host',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
