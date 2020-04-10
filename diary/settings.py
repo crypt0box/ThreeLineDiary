c85f8870d43bca39a6f5b9e8ad43e695b013fa7c
crypt0box <pray.happy76@gmail.com>
2020-04-10 14:40:08 +0900

    更新：DBをpostgresqlに変更

--- diary/settings.py
+++ diary/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'diary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'threelinediary',
+        'USER': 'cryptobox',
+        'PASSWORD': 'toshiki554798',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -145,3 +149,8 @@ if not DEBUG:
 
 # 追加
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# 追加
+db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'].update(db_from_env)
+
